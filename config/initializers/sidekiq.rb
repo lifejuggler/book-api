d63dfba81843f95c5aa3f8dad7108439597286a7
lifejuggler <jtchoiboy@gmail.com>
2017-02-09 22:11:22 -0500

    asdf

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+rails_env = Rails.env || 'development'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://clusterstuffcache.qak8xc.ng.0001.use1.cache.amazonaws.com:6379/12' }
+ config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
-
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://clusterstuffcache.qak8xc.ng.0001.use1.cache.amazonaws.com:6379/12' }
+ config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
